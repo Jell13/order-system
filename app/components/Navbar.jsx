857fcafb5a351be472450d3e711bac3b6ea6ed7b
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-08-03 13:34:40 -0700

    changed the navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className='w-full h-full md:px-44 px-12'>
       <div className='flex items-center justify-between h-full'>
-        <h1 className='text-4xl font-bold'>IFGF OC Cafe</h1>
+        <h1 className='text-4xl font-bold bg-[#EFE5DC] px-2 py-2 rounded-xl'>IFGF OC Cafe</h1>
         <MdOutlineShoppingCart size={40}/>
       </div>
     </div>
