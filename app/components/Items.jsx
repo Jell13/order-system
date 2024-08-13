90c6e390e4e58497e43fa9f17ed1634da5c8e19f
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-08-13 09:44:14 -0700

    changed to optimize the img tag

--- app/components/Items.jsx
+++ app/components/Items.jsx
@@ -8,7 +8,7 @@ const Items = ({id, name, price, image, addItem}) => {
     <div className='bg-[#EFE5DC] flex items-center justify-between px-4 py-4 rounded-xl'>
       <div className='flex flex-col gap-6'>
         <h4 className='text-2xl'>{name}</h4>
-        <img className='md:w-[200px] w-[100px]' src={image} alt="image" loading='eager'/>
+        <img className='md:w-[200px] w-[100px]' src={image} alt="image" loading='lazy'/>
       </div>
       <div className='flex flex-col gap-4'>
         <p className='bg-[#F5C5AA] px-8 py-1 rounded-xl'>
